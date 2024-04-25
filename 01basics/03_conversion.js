36d85f60e910e2ecb672ea2f1a997972c94a7f42
UttamYadav-DGI <157710495+UttamYadav-DGI@users.noreply.github.com>
2024-04-25 14:09:37 +0000

    conversionDAtatype

---
+++ 01basics/03_conversion.js
@@ -0,0 +1,24 @@
+let score="33abc"
+
+console.log(typeof(score));
+
+let valueInNumber=Number(score)//
+console.log(typeof(valueInNumber));
+console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" =>NaN
+// true =>1
+//false =>0
+let isloff=true;
+let check=Number(isloff)
+console.log(check)
+
+//1 =>true ; 0 =>false;
+//""=?false
+// "uttma" =>true
+let num=33;
+let stringnum=String(num)
+console.log(stringnum)
+console.log(typeof(stringnum))
