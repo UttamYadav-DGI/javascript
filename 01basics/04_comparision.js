f4d1fd9760900fde933db27d58f179f0c1b157f4
UttamYadav-DGI <157710495+UttamYadav-DGI@users.noreply.github.com>
2024-05-02 12:10:21 +0000

    comparision.js

---
+++ 01basics/04_comparision.js
@@ -0,0 +1,9 @@
+console.log(2>1);
+console.log("02">=1);
+
+console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
+//comparision and equaility are different things
+//===(strictly check)
+console.log("2"===2); //return false becuase the datatype is different
